a5005c93ba6a14facca0fbea9ec3d0d057a589a1
Kei178 <keisuke.inaba.cpa@gmail.com>
2021-01-06 21:10:18 +0100

    Add line bots controller

---
+++ app/controllers/line_bots_controller.rb
@@ -0,0 +1,23 @@
+class LineBotsController < ApplicationController
+  before_action :validate_signature, only: :create
+
+  def create; end
+
+  private
+
+  def validate_signature
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    return if client.validate_signature(body, signature)
+
+    render json: { result: 'Bad Request' }, status: 400
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_id = ENV['LINE_CHANNEL_ID']
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+end
